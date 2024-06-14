49f325c0542674adff62a028b9e46f1ea1ef473c
unknown <gusdudco6@naver.com>
2024-06-14 17:18:36 +0900

    ✨ Feat: 집가고 싶다
    feat: 집가고 싶다

---
+++ styling2/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+import Circle from './styled-components/sample';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Circle />
+
+    </div>
+  );
+}
+
+export default App;
