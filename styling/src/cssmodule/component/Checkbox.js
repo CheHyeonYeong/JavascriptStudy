41a2f3a224bafb50f1b6893758134e3a27275dc1
unknown <gusdudco6@naver.com>
2024-06-14 17:07:26 +0900

    🎉 Begin a new project
    Begin a new project

--- styling/src/cssmodule/component/Checkbox.js
+++ styling/src/cssmodule/component/Checkbox.js
@@ -1,10 +1,11 @@
 import React from "react";
 import {MdCheckBox, MdCheckBoxOutlineBlank} from "react-icons/md";
 import styles from "./Checkbox.module.css";
+import classNames from "classnames/bind";
 
 // 1. checkbox 생성
 // 2. checkbox icon 사용 => react-icons 설치, font awesome, ionicons, Material Design Icons 등을 쉽게 사용할 수 있게 하는 라이브러리
-
+// 3. classnames/bind 사용 => cx('클래스이름')
 
 function Checkbox({children, checked, ...rest}) {
     return (
