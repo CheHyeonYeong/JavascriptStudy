eb215fcae0c39dc24bdb2a24ef2bcc7d50adaf3a
unknown <gusdudco6@naver.com>
2024-06-17 10:47:26 +0900

    ✨ Feat: button css
    버튼 쪽 공부함 lighten 이 쪽 뭐.. 추가함

--- styling2/src/App.js
+++ styling2/src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Circle from './styled-components/sample';
-import styled from 'styled-components';
+import Button from './styled-components/Button';
+import styled, { ThemeProvider } from 'styled-components';
 
 const AppBlock = styled.div`
   width: 512px;
@@ -11,15 +12,27 @@ const AppBlock = styled.div`
   border: 1px solid black;
   padding: 1rem;
 `;
+
 function App() {
 
   return (
-    <div className="App">
-      <Circle />
+    <ThemeProvider 
+      theme={{
+        palette: {
+          blue: '#228be6',
+          gray: '#496057',
+          pink: '#f06595',
+        }
+      }}
+    >
+      {/* <Circle />
       <Circle color='pink'/>
-      <Circle color='aqua'/>
-
-    </div>
+      <Circle color='aqua'/> */}
+      <AppBlock>  {/* 자식 컴포넌트를 둘러싸고 있는 div 컴포넌트 */}
+        <Button>Button</Button>
+      </AppBlock>
+    
+    </ThemeProvider>
   );
 }
 
--- styling2/src/styled-components/sample.js
+++ styling2/src/styled-components/sample.js
@@ -1,11 +1,18 @@
 import React from "react";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+import {lighten, darken} from 'polished';
 
 const Circle = styled.div`
     width: 5rem;
     height: 5rem;
     background-color: ${props => props.color || 'black'};
     border-radius: 50%;
+    ${props => 
+        props.huge && 
+        css`            /* css는 styled-components에서 불러온 모듈 */
+        width: 10rem;
+        height: 10rem;
+    `}
 `;
 
 export default Circle;
