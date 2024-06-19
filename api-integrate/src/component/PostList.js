e0305b2bf014d65816c0c1b61c5cc5109b244e55
unknown <gusdudco6@naver.com>
2024-06-19 15:12:14 +0900

    ✨ Feat: loading, reloading
    로딩 처리랑 다시 들고오기

--- api-integrate/src/component/PostList.js
+++ api-integrate/src/component/PostList.js
@@ -3,31 +3,50 @@ import React, { useEffect, useState } from 'react';
 const Posts = () => {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // Fetch data from the API when the component mounts
+  const fetchPosts = () => {
+    setLoading(false);
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
         return response.json();
       })
       .then(data => {
         // Limit the posts to the first 20
         const limitedPosts = data.slice(0, 20);
         setPosts(limitedPosts);
+        setLoading(true);
       })
       .catch(error => {
         setError(error.message);
       });
+  };
+
+  useEffect(() => {
+    // Fetch data from the API when the component mounts
+    fetchPosts();
   }, []);
 
+  const handleReload = () => {
+    fetchPosts();
+  };
+
   if (error) {
-    return <p>Error: {error}</p>;
+    return (
+      <div>
+        <p>Error: {error}</p>
+      </div>
+    );
   }
 
-  if (error) return <div>에러가 발생했습니다.</div>;
+  if (!loading) return <h1>로딩중입니다..</h1>;
 
   return (
     <div>
+      <button onClick={handleReload}>다시 불러오기</button>
       {posts.map(post => (
         <div key={post.id}>
           <p>[{post.id}] [{post.title}] [{post.userId}]</p>
