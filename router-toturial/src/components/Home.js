fd1557de55e4088985991fe3a62bbd53eda9ac5c
unknown <gusdudco6@naver.com>
2024-06-20 10:36:47 +0900

    ✨ Router add, home, info, user
    페이지 이동 공부중

---
+++ router-toturial/src/components/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Home() {
+    return (
+        <div>
+            <ul>
+                <li><a href="/user">User</a></li>
+                <li><a href="/info">Info</a></li>
+            </ul>
+
+            <h1>Home</h1>
+        </div>
+    );
+}   
+
+export default Home;
