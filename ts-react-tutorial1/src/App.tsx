241a76335a4128bf5c897ca9e4f5cf25513134da
unknown <gusdudco6@naver.com>
2024-06-21 16:22:55 +0900

    ✨ Feat: count.tsx
    feat: count.tsx

--- ts-react-tutorial1/src/App.tsx
+++ ts-react-tutorial1/src/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 import Greetings from './component/Greetings';
+import Counter from './component/Counter';
 
 function App() {
+  const onClick = (name: string) => {
+    console.log(`${name} says hello!`);
+  }
   return (
     <div className="App">
-      <Greetings name='Hyeon'/>
+
+      <Greetings name='Hyeon' mark='!' optional='hello' onClick={onClick}/>
+      <hr />
+      <Counter />
     </div>
   );
 }
--- ts-react-tutorial1/src/component/Greetings.tsx
+++ ts-react-tutorial1/src/component/Greetings.tsx
@@ -2,11 +2,29 @@ import React from "react";
 
 type GreetingsProps = {         // Greetings에 대한 타입을 미리 지정한다.
     name: string;
+    mark?: string;
+    optional?: string;
+    onClick: (name: string) => void;
 };
 
 // const Greetings: React.FC<GreetingsProps> = ({ name }) => (<div>Hello, {name}</div>);
-  
-function Greetings({ name }: GreetingsProps) {
-    return <div>Hello, {name}</div>;
+  /* React.FC 사용시 특징
+  1. children 값이 props에 기본적으로 들어가 있음. 
+  2. defaultProps, propTypes, contextTypes 등 설정시 자동완성 처리
+  하지만, children이 옵션널로 들어가 있는 상황에서 props 타입이 명백하지 않음
+  때문에 React.FC 사용시에 Props 타입 안에 children 설정해야 함. 
+  그리고, defaultProps가 제대로 동작하지 않음. 
+*/
+const Greetings = ({name, mark, optional, onClick}:GreetingsProps) => {
+    const handleClick = () => onClick(name);
+    return(
+        <div>Hello, {name}{mark}
+            {optional && <p>{optional} </p>}
+            <div>
+                <button onClick={handleClick}>ClickMe!</button>
+            </div>
+        </div>
+    );
 }
+Greetings.defaultProps = { name: 'Hyeon', mark: '!' };
 export default Greetings;
